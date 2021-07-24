fa891362a4ad9ef3e7bff2a584a1adfb43072b45
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-07-24 10:05:30 +0200

    logged user

--- src/main/java/pl/sda/springsecurity1/controller/MainController.java
+++ src/main/java/pl/sda/springsecurity1/controller/MainController.java
@@ -2,6 +2,7 @@ package pl.sda.springsecurity1.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +14,9 @@ public class MainController {
     public String view(Model model) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
+        User principal = (User) authentication.getPrincipal();
 
+        model.addAttribute("login", principal.getUsername());
         return "index";
     }
 }
