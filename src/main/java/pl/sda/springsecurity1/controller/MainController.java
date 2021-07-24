75466b268d4c6742a08e664cb22e54a12c099386
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-07-24 11:02:48 +0200

    logowanie, wlasna formatka

--- src/main/java/pl/sda/springsecurity1/controller/MainController.java
+++ src/main/java/pl/sda/springsecurity1/controller/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping(value = {"/", "/index"})
+    @GetMapping(value = { "/index"})
     public String view(Model model) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
